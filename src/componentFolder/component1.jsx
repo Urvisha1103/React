1e3b53d8383cfd90e0038f3e44e4592e95710de7
Admin <ubaldha434@rku.ac.in>
2024-12-24 08:55:33 +0530

    2 component test

--- src/componentFolder/component1.jsx
+++ src/componentFolder/component1.jsx
@@ -1,10 +1,22 @@
 import * as React from "react";
+
+class Component2 extends React.Component {
+    render(){
+        return (
+        <div>
+            <h1>Component 2</h1>
+            <h2>world</h2>
+        </div>
+        );
+    }
+}
 class Component1 extends React.Component {
     render(){
         return (
         <div>
             <h1>Component 1</h1>
             <h2>home</h2>
+            <Component2 />
         </div>
         );
     }
